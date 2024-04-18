6dae7ab154c10361424352dd6dbdf429783f6e88
Jadi Miller <jadilmiller@gmail.com>
2024-04-17 21:15:04 -0400

    Add Rebel Girl to CD player

--- project/room/room.gd
+++ project/room/room.gd
@@ -3,3 +3,7 @@ extends Node2D
 
 func _on_bikini_girl_poster_pressed():
 	get_tree().change_scene_to_file("res://room/bikini_kill/bikini_kill.tscn")
+
+
+func _on_cd_player_pressed():
+	get_tree().change_scene_to_file("res://room/cd_player/cd_player.tscn")
